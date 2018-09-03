72172de2de1cd9b68918675cbdb7b6635099f1a3
Andrew <yueh.andrew@gmail.com>
2018-09-03 16:29:19 -0700

    All done

--- assets/js/skiathlon.js
+++ assets/js/skiathlon.js
@@ -1,10 +1,14 @@
 var frameImgs = document.getElementsByClassName('frame-clickable');
-var backdrop = document.getElementById('frame-backdrop');
+var imgContainer = document.getElementsByClassName('modal-img-container');
+var bodyTag = document.getElementsByTagName('body')
+var closeButton = document.getElementById('frame-close-button')
 
+closeButton.style.top = '20px'
 var handleImgClick = function() {
     var newClass = this.zoomed ? 'minimized-frame' : 'zoomed-frame';
     frameImgs[0].className = 'frame-clickable' + ' ' + newClass;
-    backdrop.className = this.zoomed ? 'modal' : '';
+    bodyTag[0].style.overflow = !this.zoomed ? 'hidden': 'auto'
+    imgContainer[0].style.width = this.zoomed ? '0vw' : '100vw'
     this.zoomed = !this.zoomed
 }
 
@@ -12,3 +16,5 @@ this.zoomed = false;
 for (var i = 0; i < frameImgs.length; i++) {
     frameImgs[i].addEventListener('click', handleImgClick.bind(this))
 }
+
+closeButton.addEventListener('click', handleImgClick.bind(this))
